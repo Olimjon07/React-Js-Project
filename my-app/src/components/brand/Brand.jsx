230e04bd4244dfd6e5e8bab7d3c2337111ac55bb
Olimjon07 <olimjonbotirov7@gmail.com>
2021-12-25 12:57:04 +0500

    GPT

---
+++ my-app/src/components/brand/Brand.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { google, slack, atlassian, dropbox, shopify} from './imports';
+import './brand.css'
+
+const Brand = () => {
+    return (
+        <div>
+            Brand
+        </div>
+    )
+}
+
+export default Brand
